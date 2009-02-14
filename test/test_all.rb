7587213850fa0c30046b06c276495850c5d603ff
Bernard Lambeau <blambeau@gmail.com>
2009-02-14 23:10:57 +0000

    Forces only test under test/ folder to be executed by rake.
    git-svn-id: http://svn.chefbe.net/net.chefbe.revzero/ruby/wlang/trunk@69 1f99312b-ec61-43d0-99b1-13a289e4a8e5

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,6 +1,6 @@
 require 'wlang'
 require 'test/unit'
-test_files = Dir['**/*_test.rb']
+test_files = Dir[File.join(File.dirname(__FILE__), '**/*_test.rb')]
 test_files.each { |file|
   require(file) 
 }
