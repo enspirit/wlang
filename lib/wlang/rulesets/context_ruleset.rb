8cdedb3a1315ceeb140a2119a39da5cc42e26c5f
Bernard Lambeau <blambeau@gmail.com>
2009-02-14 16:35:31 +0000

    Completes Context ruleset. Replaces modulo-assignment variant by a block-assignment rule #{...} Fixes an example bug.
    git-svn-id: http://svn.chefbe.net/net.chefbe.revzero/ruby/wlang/trunk@66 1f99312b-ec61-43d0-99b1-13a289e4a8e5

--- lib/wlang/rulesets/context_ruleset.rb
+++ lib/wlang/rulesets/context_ruleset.rb
@@ -9,7 +9,10 @@ module WLang::RuleSet::Context
   U=WLang::RuleSet::Utils
   
   # Default mapping between tag symbols and methods
-  DEFAULT_RULESET = {'=' => :assignment, '%=' => :modulo_assignment}
+  DEFAULT_RULESET = {'=' => :assignment,
+                     '#=' => :block_assignment, 
+                     '%=' => :modulo_assignment, 
+                     '^=' => :encoding_assignment}
 
   # Rule implementation of <tt>={wlang/hosted as x}{...}</tt>
   def self.assignment(parser, offset)
@@ -34,20 +37,63 @@ module WLang::RuleSet::Context
     end
   end
     
+  # Rule implementation of <tt>#={wlang/active-string}{...}{...}</tt>
+  def self.block_assignment(parser, offset)
+    variable, reached = parser.parse(offset, "wlang/active-string")
+
+    # decode expression
+    decoded = U.decode_var(variable)
+    parser.syntax_error(offset) if decoded.nil?
+    
+    # parse second block in that dialect
+    value, reached = parser.parse_block(reached)
+    
+    # handle two different cases
+    if parser.has_block?(reached)
+      parser.context_push(decoded[:variable] => value)
+      text, reached = parser.parse_block(reached)
+      parser.context_pop
+      [text, reached]
+    else
+      parser.context_define(decoded[:variable], value)
+      ["", reached]
+    end
+  end
+    
   # Rule implementation of <tt>%={wlang/active-string as x}{...}{...}</tt>
   def self.modulo_assignment(parser, offset)
     dialect_as, reached = parser.parse(offset, "wlang/ruby")
 
     # decode expression
     decoded = U.decode_qdialect_as(dialect_as)
-    if decoded.nil?
-      decoded = U.decode_var(dialect_as)
-      parser.syntax_error(offset) if decoded.nil?
-      decoded[:dialect] = nil
-    end
+    parser.syntax_error(offset) if decoded.nil?
     
     # parse second block in that dialect
     value, reached = parser.parse_block(reached, decoded[:dialect])
+    
+    # handle two different cases
+    if parser.has_block?(reached)
+      parser.context_push(decoded[:variable] => value)
+      text, reached = parser.parse_block(reached)
+      parser.context_pop
+      [text, reached]
+    else
+      parser.context_define(decoded[:variable], value)
+      ["", reached]
+    end
+  end
+    
+  # Rule implementation of <tt>^={wlang/active-string as x}{...}{...}</tt>
+  def self.encoding_assignment(parser, offset)
+    encoding_as, reached = parser.parse(offset, "wlang/ruby")
+
+    # decode expression
+    decoded = U.decode_qencoder_as(encoding_as)
+    parser.syntax_error(offset) if decoded.nil?
+    
+    # parse second block in that dialect
+    value, reached = parser.parse_block(reached)
+    value = parser.encode(value, decoded[:encoder])
     
     # handle two different cases
     if parser.has_block?(reached)
