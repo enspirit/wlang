87244ebd155ca90ac3c0f2b7e37ab17188a3a313
Bernard Lambeau <blambeau@gmail.com>
2011-01-13 08:26:25 +0100

    Moved to RSpec 2.4.0

--- test/spec/test_all.rb
+++ test/spec/test_all.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
 require 'wlang'
-require 'spec/autorun'
+require 'rspec/autorun'
 test_files = Dir[File.join(File.dirname(__FILE__), '**/*.spec')]
 test_files.each { |file|
   load(file) 
