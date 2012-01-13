5d585b3207d2e73af81d2830320c27b54c5c0939
Bernard Lambeau <blambeau@gmail.com>
2012-01-13 13:52:10 +0100

    Enhance parser test coverage

--- spec/test_parser.rb
+++ spec/test_parser.rb
@@ -18,5 +18,22 @@ module WLang
       parser.call("Hello ${world}!").should eq(expected)
     end
     
+    it 'should support high-order wlang' do
+      expected = \
+        [:wlang,  "$",
+          [:fn, [:wlang, "$", [:fn, [:static, "who"]]]]
+        ]
+      parser.call("${${who}}").should eq(expected)
+    end
+    
+    it 'should support mutli-block functions' do
+      expected = \
+        [:wlang,  "$",
+          [:fn, [:static, "first" ]],
+          [:fn, [:static, "second"]],
+        ]
+      parser.call("${first}{second}").should eq(expected)
+    end
+    
   end
 end
