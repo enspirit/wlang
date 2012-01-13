9b96876d85c0f9d27fb88accaa5c5c9c5c93394b
Bernard Lambeau <blambeau@gmail.com>
2012-01-13 14:03:04 +0100

    Temple expect an options argument

--- lib/wlang/parser.rb
+++ lib/wlang/parser.rb
@@ -1,6 +1,9 @@
 module WLang
   Citrus.load(File.expand_path('../grammar', __FILE__))
   class Parser
+    
+    def initialize(options = {})
+    end
 
     def call(input)
       WLang::Grammar.parse(input).value
