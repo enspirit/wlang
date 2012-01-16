4e8fdd20575bad435af65b8b04ea9af75e8220ea
Bernard Lambeau <blambeau@gmail.com>
2012-01-16 17:14:06 +0100

    TDD on Dialect#instantiate

--- spec/unit/dialect/test_instantiate.rb
+++ spec/unit/dialect/test_instantiate.rb
@@ -4,17 +4,20 @@ module WLang
     
     class Upcasing < Dialect
       
-      def upcasing(fn, *rest)
+      rule "!", do |fn, *rest|
         fn.call(self, "").upcase
       end
       
-      rule "!", :upcasing
     end
     
     let(:upcasing){ Upcasing.new }
     
     it 'works as expected' do
       upcasing.instantiate("Hello !{who}").should eq("Hello WHO")
+    end
+    
+    it 'do not eat extra blocks' do
+      upcasing.instantiate("Hello !{who}{world}").should eq("Hello WHO{world}")
     end
     
   end # describe Dialect
