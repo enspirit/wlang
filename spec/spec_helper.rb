5202db7fbf44bc3937e790029098ec6c6da8dccc
Bernard Lambeau <blambeau@gmail.com>
2012-01-18 10:14:08 +0100

    Introduce spec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,38 @@
 require 'epath'
-root = Path.backfind('.[Rakefile]')
 
-$LOAD_PATH.unshift root/:lib
-$LOAD_PATH.unshift root/:spec
+module Helpers
+  
+  def root_folder
+    @root_folder ||= Path.backfind('.[Rakefile]')
+  end
+  
+  def spec_folder
+    root_folder/:spec
+  end
+  
+  def tpl_path(what)
+    what = "#{what}.tpl" if what.is_a?(Symbol)
+    spec_folder/:fixtures/:templates/what
+  end
+  
+  def tpl(what)
+    tpl_path.read
+  end
+  
+end
+include Helpers
 
+# Require wlang
+$LOAD_PATH.unshift root_folder/:lib
+$LOAD_PATH.unshift root_folder/:spec
 require 'wlang'
 
-(root/:spec).glob("fixtures/dialect/*.rb").each do |f|
+# Load fixture dialects
+spec_folder.glob("fixtures/dialect/*.rb").each do |f|
   require f.expand_path
+end
+
+# Configure rspec
+RSpec.configure do |c|
+  c.include Helpers
 end
