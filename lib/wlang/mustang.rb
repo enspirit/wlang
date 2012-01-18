d31d1283a47653c4a21204e99cd145ea4e7184c5
Bernard Lambeau <blambeau@gmail.com>
2012-01-18 17:55:36 +0100

    Mustiche fixture becomes Mustang dialect

---
+++ lib/wlang/mustang.rb
@@ -0,0 +1,55 @@
+module WLang
+  class Mustang < WLang::Dialect
+    include Temple::Utils
+  
+    def plus(fn)
+      varname = instantiate(fn)
+      known?(varname) ? evaluate(varname) : nil
+    end
+    tag '+', :plus
+  
+    def escape(fn)
+      escape_html plus(fn)
+    end
+    tag '$', :escape
+    tag '&', :escape
+  
+    def section(fn1, fn2)
+      case x = plus(fn1)
+      when FalseClass, NilClass
+        nil
+      when Array
+        x.inject(""){|buf,item|
+          buf << with_scope(item){ instantiate(fn2) }
+        }
+      when Proc
+        x.call lambda{ instantiate(fn2) }
+      else
+        with_scope(x){ instantiate(fn2) }
+      end
+    end
+    tag '#', :section
+  
+    def inverted(fn1, fn2)
+      case x = plus(fn1)
+      when FalseClass, NilClass
+        instantiate(fn2)
+      when Array
+        instantiate(fn2) if x.empty?
+      end
+    end
+    tag '^', :inverted
+  
+    def comment(fn)
+    end
+    tag '!', :comment
+  
+    def partial(fn)
+      if x = plus(fn)
+        instantiate(x)
+      end
+    end
+    tag '>', :partial
+  
+  end # class Mustang
+end # module WLang
