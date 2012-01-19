eaee1c442f42fc7fdb886c0e39b0345712d32728
Bernard Lambeau <blambeau@gmail.com>
2012-01-19 10:39:57 +0100

    Move temple abstractions under compiler

--- lib/wlang.rb
+++ lib/wlang.rb
@@ -29,8 +29,8 @@ module WLang
   
 end # module WLang
 require 'wlang/scope'
-require 'wlang/parser'
-require 'wlang/compiler'
-require 'wlang/generator'
+require 'wlang/compiler/parser'
+require 'wlang/compiler/compiler'
+require 'wlang/compiler/generator'
 require 'wlang/template'
 require 'wlang/dialect'
