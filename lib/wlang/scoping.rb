5c0a3075b21c27d9ad9f54badded2f86fb560d45
Bernard Lambeau <blambeau@gmail.com>
2012-01-19 16:09:55 +0100

    Merge Strict inside Scoping

--- lib/wlang/scoping.rb
+++ lib/wlang/scoping.rb
@@ -1,1 +1,20 @@
-require 'wlang/scoping/strict'
+module WLang
+  module Scoping
+    module Strict
+
+      attr_reader :scope
+
+      def with_scope(x)
+        old, @scope = @scope, x
+        yield
+      ensure
+        @scope = old
+      end
+
+      def each_scope
+        block_given? ? yield(scope) : [scope].compact.each
+      end
+
+    end # module Strict
+  end # module Scoping
+end # module WLang
