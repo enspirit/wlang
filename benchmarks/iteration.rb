25cf12df120f150fa87b744651636f4a9ee75c54
Bernard Lambeau <blambeau@gmail.com>
2012-01-19 08:52:32 +0100

    Add ERB to benchmark comparison

--- benchmarks/iteration.rb
+++ benchmarks/iteration.rb
@@ -1,10 +1,11 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'wlang/mustang'
 require 'benchmark'
+require 'erb'
 require 'mustache'
 
-max      = 50000
-scope    = { :range => (1..max).map{|i| {:i => i}} }
+max      = 20000
+scope = { :range => (1..max).map{|i| {:i => i}} }
 
 Benchmark.bm(10) do |x|
   x.report(:wlang){
@@ -12,6 +13,10 @@ Benchmark.bm(10) do |x|
   }
   x.report(:mustache){
     Mustache.render('{{#range}}{{i}}, {{/range}}', scope)
+  }
+  x.report(:erb){
+    tpl = ERB.new(%Q{<% (1..max).each do |i| %><%= i %>, <% end %>})
+    tpl.result
   }
   x.report(:native){
     buf = ""
