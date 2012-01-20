3eb73d566ea0514f00d073dba2e7ad9d53d0fb8e
Bernard Lambeau <blambeau@gmail.com>
2012-01-20 17:47:39 +0100

    Prepare filter to be used as a module as well.

--- lib/wlang/compiler/filter.rb
+++ lib/wlang/compiler/filter.rb
@@ -1,20 +1,39 @@
 module WLang
   class Compiler
     class Filter < Temple::Filter
+      
+      module ClassMethods
 
-      def self.recurse_on(*kinds)
-        kinds.each do |kind|
-          define_method(:"on_#{kind}") do |*args|
-            recurse(kind, *args)
+        def recurse_on(*kinds)
+          kinds.each do |kind|
+            define_method(:"on_#{kind}") do |*args|
+              recurse(kind, *args)
+            end
           end
         end
+        
       end
+      extend ClassMethods
       
-      def recurse(kind, *args)
-        args.inject [kind] do |rw,arg|
-          rw << (arg.is_a?(Array) ? call(arg) : arg)
+      module InstanceMethods
+
+        def recurse(kind, *args)
+          args.inject [kind] do |rw,arg|
+            rw << (arg.is_a?(Array) ? call(arg) : arg)
+          end
         end
+
       end
+      include InstanceMethods
+      
+      module Helpers
+        
+        def self.included(mod)
+          mod.instance_eval{ include(Filter::InstanceMethods) }
+          mod.extend(Filter::ClassMethods)
+        end
+        
+      end # module Helpers
 
     end # class Filter
   end # class Compiler
