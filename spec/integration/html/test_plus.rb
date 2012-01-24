30f09f2434ae6dfc8dbe79ac62d3c25a9da8b3dc
Bernard Lambeau <blambeau@gmail.com>
2012-01-24 11:42:53 +0100

    Ensure +{Proc} calls to_html on the result

--- spec/integration/html/test_plus.rb
+++ spec/integration/html/test_plus.rb
@@ -3,21 +3,24 @@ require 'wlang/html'
 module WLang
   describe Html, "+{...}" do
 
+    ToHtml = Struct.new(:to_html)
+    ToS    = Struct.new(:to_s)
+
     def render(tpl, scope = {})
       Html.render(tpl, scope, "")
     end
 
     context "on an aribitraty object" do
       it 'invokes to_html if it exists' do
-        hello = Struct.new(:to_html).new("<h1>World</h1>")
+        hello = ToHtml.new("<h1>World</h1>")
         render("+{hello}", binding).should eq("<h1>World</h1>")
       end
       it 'invokes to_s if to_html does not exists' do
-        hello = Struct.new(:to_s).new("World")
+        hello = ToS.new("World")
         render("+{hello}", binding).should eq("World")
       end
       it 'invokes to_s on the result of to_html' do
-        hello = Struct.new(:to_html).new(Struct.new(:to_s).new("<h1>World</h1>"))
+        hello = ToHtml.new(ToS.new("<h1>World</h1>"))
         render("+{hello}", binding).should eq("<h1>World</h1>")
       end
     end
@@ -33,6 +36,10 @@ module WLang
     context "on a Proc" do
       it 'calls it if of arity 0' do
         hello = Proc.new{ "World" }
+        render("+{hello}!", binding).should eq("World!")
+      end
+      it 'it invokes to_html on the result' do
+        hello = Proc.new{ ToHtml.new("World") }
         render("+{hello}!", binding).should eq("World!")
       end
     end
